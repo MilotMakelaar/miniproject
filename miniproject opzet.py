4d527f7e60be8902ffa9eaa109352aefb2d932b0
Milot <milot.makelaar@student.hu.nl>
2019-03-26 16:19:54 +0100

    Initial commit

---
+++ miniproject opzet.py
@@ -0,0 +1,19 @@
+aanb_bez = input('Bent u een aanbieder of bezoeker?')
+import.
+if aanb_bez == aanbieder:
+    naam_aan = input('Vul hier uw naam in: ')
+    email_aan = input('Vul hier uw email in: ')
+    print('Klopt dit? Ja/nee')
+    ant = input()
+    print('1. Overzicht films die nog niet aangeboden wordt door ander aanbieder.')
+    print('2. Overzicht films die de aanbieder al aanbiedt.')
+    keuze_aan = input('Kies optie: ')
+elif keuze_aan == 1:
+    ..
+elif keuze_aan == 2:
+    ..
+else:
+    naam_bez = input('Vul hier uw naam in: ')
+    email_bez = input('Vul hier uw email in: ')
+    print('Klopt dit? Ja / nee')
+    antw = input()
